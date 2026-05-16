af6dd133a70c7c5a029608b36ac374271e28658c
Teero888 <teero484@gmail.com>
2026-05-16 17:57:33 +0200

    fix utf8 open

--- ddnet_map_loader.c
+++ ddnet_map_loader.c
@@ -5,6 +5,19 @@
 #include <string.h>
 #include <zlib.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#include <wchar.h>
+static FILE *local_fopen(const char *path, const char *mode) {
+  wchar_t wpath[1024];
+  wchar_t wmode[16];
+  if (MultiByteToWideChar(CP_UTF8, 0, path, -1, wpath, 1024) <= 0) return NULL;
+  if (MultiByteToWideChar(CP_UTF8, 0, mode, -1, wmode, 16) <= 0) return NULL;
+  return _wfopen(wpath, wmode);
+}
+#define fopen local_fopen
+#endif
+
 // All the typedefs for datafile structures are unchanged...
 typedef struct datafile_item_type_t {
   int type;
